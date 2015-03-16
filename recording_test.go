82145e74affef85cda2e06bd07b0df67a503eed4
Thorsten von Eicken <tve@rightscale.com>
2015-03-16 11:54:49 -0700

    switch to gomega's ghttp test handler

--- recording_test.go
+++ recording_test.go
@@ -8,14 +8,15 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	//"io/ioutil"
 	"net/http"
-	"net/http/httptest"
+	//"net/http/httptest"
 	"os"
 	"strings"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/ghttp"
 )
 
 // Iterate through all recorded test cases and play them back
@@ -38,31 +39,48 @@ var _ = Describe("Recorded request", func() {
 			break
 		}
 
-		// Create an http handler function that checks that we're indeed getting the
-		// expected request and then returns the recorded response
-		handler := func(w http.ResponseWriter, req *http.Request) {
-			defer GinkgoRecover()
-			reqBody, _ := ioutil.ReadAll(req.Body)
-			Ω(req.Method).Should(Equal(testCase.RR.Verb))
-			Ω(req.URL).Should(Equal(testCase.RR.Uri))
-			Ω(reqBody).Should(Equal(testCase.RR.ReqBody))
-			for k, _ := range testCase.RR.ReqHeader {
-				Ω(req.Header.Get(k)).Should(Equal(testCase.RR.ReqHeader.Get(k)))
-			}
+		/*
+			handler := func(w http.ResponseWriter, req *http.Request) {
+				reqBody, _ := ioutil.ReadAll(req.Body)
+				Ω(req.Method).Should(Equal(testCase.RR.Verb))
+				Ω(req.URL).Should(Equal(testCase.RR.Uri))
+				Ω(reqBody).Should(Equal(testCase.RR.ReqBody))
+				for k, _ := range testCase.RR.ReqHeader {
+					Ω(req.Header.Get(k)).Should(Equal(testCase.RR.ReqHeader.Get(k)))
+				}
 
-			w.WriteHeader(testCase.RR.Status)
-			w.Write([]byte(testCase.RR.RespBody))
-		}
+				w.WriteHeader(testCase.RR.Status)
+				w.Write([]byte(testCase.RR.RespBody))
+			}
+		*/
 
 		// Perform the test by running main() with the command line args set
 		It(strings.Join(testCase.CmdArgs, " "), func() {
-			server := httptest.NewServer(http.HandlerFunc(handler))
+			//server := httptest.NewTLSServer(http.HandlerFunc(handler))
+			server := ghttp.NewServer()
 			defer server.Close()
+
+			// construct list of verifiers
+			verifiers := []http.HandlerFunc{
+				ghttp.VerifyRequest(testCase.RR.Verb, testCase.RR.Uri),
+				ghttp.VerifyRequest("HELLO", testCase.RR.Uri),
+			}
+			if len(testCase.RR.ReqBody) > 0 {
+				verifiers = append(verifiers,
+					ghttp.VerifyJSON(testCase.RR.ReqBody))
+			}
+			for k, _ := range testCase.RR.ReqHeader {
+				verifiers = append(verifiers,
+					ghttp.VerifyHeaderKV(k, testCase.RR.ReqHeader.Get(k)))
+			}
+			verifiers = append(verifiers,
+				ghttp.RespondWith(testCase.RR.Status, testCase.RR.RespBody))
+			server.AppendHandlers(verifiers...)
 
 			os.Args = append([]string{
 				"rs-api", "--rl10",
 				//"--debug",
-				"--host", strings.TrimPrefix(server.URL, "http://")},
+				"--host", strings.TrimPrefix(server.URL(), "http://")},
 				testCase.CmdArgs...)
 			fmt.Fprintf(os.Stderr, "testing \"%s\"\n", strings.Join(os.Args, `" "`))
 
